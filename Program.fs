50fe77fdb5b368c9e169410ed29e26fc7ade0b5b
mkolibaba <avierdaner@gmail.com>
2018-12-01 12:20:14 +0300

    Solve 1.1 and begin 1.2

---
+++ Program.fs
@@ -0,0 +1,41 @@
+﻿// Learn more about F# at http://fsharp.org
+
+module aoc2018
+
+open System
+open System.IO
+
+module Utils =
+    let readInput day = (sprintf "resources\\input-%s.txt" day) |> File.ReadLines |> List.ofSeq
+    let splitBy (splitter: string) (str: string) = str.Split([|splitter|], StringSplitOptions.None) |> List.ofArray
+    let joinBy (joiner: string) (l: 'a list) = String.Join(joiner, l)
+
+module Day1 = 
+    let solve freqs (input: string list) = 
+        let folder freqs' value = freqs'@[List.last freqs' + value]
+        input |> List.map Int32.Parse |> List.fold folder freqs
+    let solve1 = solve [0] >> List.last 
+    let solve2 input =
+        let rec solve2' solved =
+            match solved |> List.countBy id |> List.tryFind (fun x -> snd x > 1) with
+            | Some twice -> twice |> fst
+            | None -> solve2' (solve solved input)
+        solve2' (solve [0] input)    
+
+    let decide = function | "2" -> solve2 | "1" | _ -> solve1
+
+[<EntryPoint>]
+let main argv =
+    let day = argv |> Array.head
+    let part = argv |> Array.tail |> Array.head
+
+    let decider day = 
+        match day with
+        | "1" -> Day1.decide
+        | _ -> failwith "wrong day"
+    
+    let solver = part |> (decider day)
+
+    day |> Utils.readInput |> solver |> printfn "%A"
+
+    0
